033bff8768883ae65e6d0ca52398fe43fe357711
joaodois <88596300+joaodois@users.noreply.github.com>
2022-02-10 15:44:15 -0300

    Add files via upload

---
+++ C24v1PRO---projeto-aluno/sketch.js
@@ -0,0 +1,36 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var stone, rubber,hammer;
+
+
+function setup(){
+    var canvas = createCanvas(1200,600);
+    engine = Engine.create();
+    world = engine.world;
+
+    plane = new Plane(600,height,1200,20);
+    iron = new Iron(300,350);
+    stone = new Stone(700,320,100,100);
+    rubber = new Rubber(900,450,70);
+  
+  
+
+
+}
+
+function draw(){
+    background("lightBlue");
+    Engine.update(engine);
+
+    stone.display();
+    plane.display();
+    rubber.display();
+
+    iron.display();  
+   
+
+ 
+}
